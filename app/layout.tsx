d0565da0e7ec5b437d25381cc4341c7bdab315e5
Oumarou Sanda Souley <148893596+OumarouSandaSouley@users.noreply.github.com>
2025-04-08 15:00:20 +0100

    unused import and code removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,11 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className=""
-      >
-        {children}
-      </body>
+      <body className="">{children}</body>
     </html>
   );
 }
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import { cookies } from "next/headers";
 import { Database } from "./types";
 
 export async function createSupabaseClient() {
-  const cookieStore = cookies();
-
   return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
